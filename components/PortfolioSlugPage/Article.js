2f70f938cdf087b14e5c7ecd359d47864fb3af31
James Wallis <james.wallis1@ibm.com>
2020-07-03 20:25:20 +0100

    Add back to portfolio button

--- components/PortfolioSlugPage/Article.js
+++ components/PortfolioSlugPage/Article.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import DateFormatter from '../Generic/DateFormatter'
 import PrintMarkdownHTML from '../Generic/PrintMarkdownHTML'
 
@@ -15,11 +16,16 @@ export default function Article({ title, image, date, content }) {
         alt={`Image for ${title}`}
         className="md:h-50vh"
       />
-      <h1 className="text-6xl font-title text-pink-custom mt-4 md:mt-16 mb-4">{title}</h1>
+      <h1 className="text-4xl md:text-6xl font-title text-pink-custom mt-4 md:mt-16 mb-4">{title}</h1>
       <p className="font-title my-2 md:my-8 text-lg">
         <DateFormatter dateString={date} />
       </p>
       <PrintMarkdownHTML content={content} />
+      <Link href={`${process.env.BACKEND_URL}/portfolio`}>
+        <a className="px-4 py-2 my-8 md:my-8 border-solid border border-black font-body text-base hover:bg-black hover:text-pink-custom ease-in-out transition duration-500">
+          Back to Portfolio
+        </a>
+      </Link>
     </article>
   )
 }
